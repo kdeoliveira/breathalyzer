742309c0975867c09c2a992b40e6840538947d41
antoinegaubil <antoinegaubil@gmail.com>
2022-05-29 17:56:22 -0400

    DashBoard, sprint 1

--- Abreath/app/src/main/java/com/coen390/abreath/ui/dashboard/DashboardViewModel.java
+++ Abreath/app/src/main/java/com/coen390/abreath/ui/dashboard/DashboardViewModel.java
@@ -7,13 +7,53 @@ import androidx.lifecycle.ViewModel;
 public class DashboardViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
+    private final MutableLiveData<String> Username;
+    private final MutableLiveData<String> DisplayData;
+    String username = "xxx";
+
+    float userdata = 0.08f;
+    float threshold = 0.08f;
 
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        Username = new MutableLiveData<>();
+        DisplayData= new MutableLiveData<>();
+
+        DisplayData.setValue(userdata + "mg/L");
+        Username.setValue(username + ", the results are out :");
+
+        if(userdata > threshold)
+        {
+            mText.setValue(username + ", you are above the legal limit! \n Please do not take the wheel.");
+        }
+        else if(userdata < threshold)
+        {
+            mText.setValue(username + ", you are under the legal limit. \n You are good to drive! ");
+        }
+        else
+        {
+            mText.setValue("Test Unsuccessful.\n Please try again.");
+        }
+
     }
+
+
+
+
 
     public LiveData<String> getText() {
         return mText;
     }
+    public LiveData<String> getUsername() {
+        return Username;
+    }
+    public LiveData<String> getData() {
+        return DisplayData;
+    }
+    public float Data() {
+        return userdata;
+    }
+
+
+
 }
