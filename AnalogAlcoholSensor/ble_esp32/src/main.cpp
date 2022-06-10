48ae54fc5e3070a880dcc7a243a2840d1d2ce304
Kevin de Oliveira <contact@kevindeoliveira.com>
2022-06-10 14:07:38 -0400

    Adjust colors of graphs hBar

--- AnalogAlcoholSensor/ble_esp32/src/main.cpp
+++ AnalogAlcoholSensor/ble_esp32/src/main.cpp
@@ -9,26 +9,26 @@
 
 uint8_t ledR = 2;
 uint8_t ledG = 4;
-uint8_t ledB = 5; 
+uint8_t ledB = 5;
 
 uint8_t ledArray[3] = {1, 2, 3}; // three led channels
 
-uint8_t color = 0;          // a value from 0 to 255 representing the hue
-uint32_t R, G, B;           // the Red Green and Blue color components
-uint8_t brightness = 255;  // 255 is maximum brightness, but can be changed.  Might need 256 for common anode to fully turn off.
+uint8_t color = 0;        // a value from 0 to 255 representing the hue
+uint32_t R, G, B;         // the Red Green and Blue color components
+uint8_t brightness = 255; // 255 is maximum brightness, but can be changed.  Might need 256 for common anode to fully turn off.
 
 int drunk = 2000; // threshold of drunk
-int peak = 0; //peak value
-int prepeak = 0; //previous peak value
+int peak = 0;     // peak value
+int prepeak = 0;  // previous peak value
 
-std::__cxx11::string message = "";//message from app
-char incomingChar; // message from app
+std::__cxx11::string message = ""; // message from app
+char incomingChar;                 // message from app
 
 int analogValue = 0;
-bool Flag = false; //flag for start measure
-int counter = 0; // counter if has reach the peak value
+bool Flag = false; // flag for start measure
+int counter = 0;   // counter if has reach the peak value
 
-float hum;
+float valueToSend;
 
 // Timer variables
 unsigned long lastTime = 0;
@@ -64,12 +64,22 @@ class MyCharacteristicCallback : public BLECharacteristicCallbacks
   void onWrite(BLECharacteristic *pCharacteristic, esp_ble_gatts_cb_param_t *param)
   {
     isReadyToSend = true;
-    message = pCharacteristic->getValue();    
+    message = pCharacteristic->getValue();
   }
 };
 
 BLECharacteristic *pCharacteristic;
 BLEServer *pServer;
+
+
+void sendToBluetooth(int val){
+  float num = static_cast<float>(val);
+  static char buff[3];
+  dtostrf(num, 5, 2, buff);
+  pCharacteristic->setValue(buff);
+  pCharacteristic->notify();
+}
+
 void setup()
 {
   Serial.begin(115200);
@@ -96,17 +106,16 @@ void setup()
   BLEDevice::startAdvertising();
   Serial.println("Characteristic defined! Now you can read it in your phone!");
   // hardware setting
-  analogReadResolution(12); //adc resolution
+  analogReadResolution(12); // adc resolution
 
   ledcAttachPin(ledR, 1); // assign RGB led pins to channels
   ledcAttachPin(ledG, 2);
   ledcAttachPin(ledB, 3);
-  
-  // Initialize channels 
+
+  // Initialize channels
   ledcSetup(1, 12000, 8); // 12 kHz PWM, 8-bit resolution
   ledcSetup(2, 12000, 8);
   ledcSetup(3, 12000, 8);
-  
 }
 
 void loop()
@@ -115,81 +124,103 @@ void loop()
   if (deviceConnected)
   {
     Serial.println("deviceConnected");
-    if ((millis() - lastTime) > timerDelay) {
-    // Generating random numbers
+    if ((millis() - lastTime) > timerDelay)
+    {
 
-  //check from app for starting
-  if(message == "M") 
-  Flag = true;
+      // check from app for starting
+      if (message == "M")
+      {
+        // Generating random numbers
+        Flag = true;
+      }
 
-  // measuring
-  if(Flag){
-  // read the analog / millivolts value for pin 2:
-  analogValue = analogRead(14);
-  Serial.println(analogValue);
-  //store peak value
-  if (prepeak>=analogValue){
-  peak = prepeak;
-  counter++;
-  }
-  else{
-  peak = analogValue;
-  prepeak = peak;
-  counter = 0;}
-  //
-  if(counter<20&&counter!=0){
-  R=255;
-  G=255;
-  B=255;
-  //white
-  ledcWrite(1, R); // write red component to channel 1, etc.
-  ledcWrite(2, G);   
-  ledcWrite(3, B); 
-  //send peak
-  if(isReadyToSend){
-  static char buff[3];
-  itoa(peak,buff, 6);
-  pCharacteristic->setValue(buff);
-  pCharacteristic->notify();  
-  }
-  } 
-  //has reach the peak value
-  else{
-  if(isReadyToSend){
-  static char buff[3];
-  itoa(9999,buff, 6);
-  pCharacteristic->setValue(buff);
-  pCharacteristic->notify();  
-  isReadyToSend = false;
-  }
-  counter = 0;
-  prepeak = 0;
-  Flag = false;
-  if(peak > drunk){
-  R=255;
-  G=0;
-  B=0;
-  ledcWrite(1, R); // write red component to channel 1, etc.
-  ledcWrite(2, G);   
-  ledcWrite(3, B); 
-  }
-  else{
-  R=0;
-  G=255;
-  B=0;
-  ledcWrite(1, R); // write red component to channel 1, etc.
-  ledcWrite(2, G);   
-  ledcWrite(3, B); 
-  }
-  }
-  prevDeviceConnected = true;
-  lastTime = millis();
+      // measuring
+      if (Flag)
+      {
+        // read the analog / millivolts value for pin 2:
+        analogValue = analogRead(14);
+        Serial.println(analogValue);
+
+
+
+
+        analogValue = 0;
+        
+        // store peak value
+        if (prepeak >= analogValue)
+        {
+          peak = prepeak;
+          counter++;
+        }
+        else
+        {
+          peak = analogValue;
+          prepeak = peak;
+          counter = 0;
+        }
+        //
+        if (counter < 20 && counter != 0)
+        {
+          R = 255;
+          G = 255;
+          B = 255;
+          // white
+          ledcWrite(1, R); // write red component to channel 1, etc.
+          ledcWrite(2, G);
+          ledcWrite(3, B);
+          sendToBluetooth(20);
+          isReadyToSend = false;
+          
+          // send peak
+          if (isReadyToSend)
+          {
+            // static char buff[3];
+            // itoa(peak, buff, 6);
+            // pCharacteristic->setValue(buff);
+            // pCharacteristic->notify();
+          }
+        }
+        // has reach the peak value
+        else
+        {
+          if (isReadyToSend)
+          {
+            // static char buff[3];
+            // itoa(9999, buff, 6);
+            // pCharacteristic->setValue(buff);
+            // pCharacteristic->notify();
+            // isReadyToSend = false;
+          }
+          counter = 0;
+          prepeak = 0;
+          Flag = false;
+          if (peak > drunk)
+          {
+            R = 255;
+            G = 0;
+            B = 0;
+            ledcWrite(1, R); // write red component to channel 1, etc.
+            ledcWrite(2, G);
+            ledcWrite(3, B);
+          }
+          else
+          {
+            R = 0;
+            G = 255;
+            B = 0;
+            ledcWrite(1, R); // write red component to channel 1, etc.
+            ledcWrite(2, G);
+            ledcWrite(3, B);
+          }
+        }
+      }
+      prevDeviceConnected = true;
+      lastTime = millis();
     }
-  }
   }
   else if (!deviceConnected && prevDeviceConnected)
   {
-        Serial.println("!deviceConnected && prevDeviceConnected");
+    Serial.println("!deviceConnected && prevDeviceConnected");
     delay(500);
     pServer->startAdvertising(); // Restart scanning
     Serial.println("Scanning");
@@ -197,9 +228,8 @@ void loop()
   }
   else if (deviceConnected && !prevDeviceConnected)
   {
-        Serial.println("!deviceConnected && !prevDeviceConnected");
+    Serial.println("!deviceConnected && !prevDeviceConnected");
     prevDeviceConnected = deviceConnected;
     Serial.println("Connecting...");
   }
-  
 }
